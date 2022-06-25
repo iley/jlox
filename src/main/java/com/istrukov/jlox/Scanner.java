1dff883615db9851e792f8037c9262546e882fe7
Ilya Strukov <iley@iley.ru>
2022-06-25 19:23:43 +0100

    Complete lexer

--- src/main/java/com/istrukov/jlox/Scanner.java
+++ src/main/java/com/istrukov/jlox/Scanner.java
@@ -1,5 +1,7 @@
 package com.istrukov.jlox;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,25 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final ImmutableMap<String, TokenType> keywords = ImmutableMap.<String, TokenType>builder()
+            .put("and", TokenType.AND)
+            .put("class", TokenType.CLASS)
+            .put("else", TokenType.ELSE)
+            .put("false", TokenType.FALSE)
+            .put("for", TokenType.FOR)
+            .put("fun", TokenType.FUN)
+            .put("if", TokenType.IF)
+            .put("nil", TokenType.NIL)
+            .put("or", TokenType.OR)
+            .put("print", TokenType.PRINT)
+            .put("return", TokenType.RETURN)
+            .put("super", TokenType.SUPER)
+            .put("this", TokenType.THIS)
+            .put("true", TokenType.TRUE)
+            .put("var", TokenType.VAR)
+            .put("while", TokenType.WHILE)
+            .build();
 
     public Scanner(String input) {
         this.input = input;
@@ -57,13 +78,114 @@ public class Scanner {
             case '*':
                 addToken(TokenType.STAR);
                 break;
+            case '!':
+                addToken(match('=') ? TokenType.BANG_EQUAL: TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(TokenType.SLASH);
+                }
+                break;
+            case ' ':
+            case '\t':
+            case '\r':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                scanString();
+                break;
             default:
-                Lox.error(line, String.format("Unexpected character %s", c));
+                if (isDigit(c)) {
+                    scanNumber();
+                } else if (isAlpha(c)) {
+                    scanIdentifier();
+                } else {
+                    Lox.error(line, String.format("Unexpected character %s", c));
+                }
         }
     }
 
+    private void scanIdentifier() {
+        while (isAlphanumeric(peek())) {
+            advance();
+        }
+        var text = input.substring(start, current);
+        var type = keywords.getOrDefault(text, TokenType.IDENTIFIER);
+        addToken(type);
+    }
+
+    private  void scanString() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "unterminated string");
+            return;
+        }
+        advance(); // terminating '"'
+        var value = input.substring(start+1, current-1);
+        addToken(TokenType.STRING, new Literal(value));
+    }
+
+    private void scanNumber() {
+        while (isDigit(peek())) {
+            advance();
+        }
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance(); // '.'
+            while (isDigit(peek())) {
+                advance();
+            }
+        }
+
+        var value = Double.parseDouble(input.substring(start, current));
+        addToken(TokenType.NUMBER, new Literal(value));
+    }
+
     private char advance() {
+        if (isAtEnd()) {
+            return '\0';
+        }
         return input.charAt(current++);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) {
+            return false;
+        }
+        if (input.charAt(current) != expected) {
+            return false;
+        }
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) {
+            return '\0';
+        }
+        return input.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current+1 >= input.length()) {
+            return '\0';
+        }
+        return input.charAt(current+1);
     }
 
     private void addToken(TokenType type) {
@@ -78,5 +200,17 @@ public class Scanner {
 
     private boolean isAtEnd() {
         return current >= input.length();
+    }
+
+    private static boolean isDigit(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private static boolean isAlpha(char c) {
+        return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || c == '_';
+    }
+
+    private static boolean isAlphanumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 }
